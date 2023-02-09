efa0ce72f626465d2f663578b0f8fa4b34ecb177
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-02-09 15:47:15 +0900

    add: users

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,26 @@
+class UserController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render json: user
+  end
+
+  def create
+    user = User.create(params.permit(:name))
+    render json: user
+  end
+
+  def update
+    id = params[:id]
+    user = User.find(id)
+    user.update(params.permit(:name))
+    render json: user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # users
+  get "/users", to: "user#index"
+  get "/users/:id", to: "user#show"
+  post "/users", to: "user#create"
+  patch "/users/:id", to: "user#update"
 end
