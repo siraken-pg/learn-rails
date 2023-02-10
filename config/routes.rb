cc64b7e71e9e141f4b38dd361cb06c63e63e5482
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-02-10 12:33:21 +0900

    feat: grouped routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # users
-  get "/users", to: "user#index"
-  get "/users/:id", to: "user#show"
-  post "/users", to: "user#create"
-  patch "/users/:id", to: "user#update"
+  scope :api do
+    # users
+    get "/users", to: "user#index"
+    get "/users/:id", to: "user#show"
+    post "/users", to: "user#create"
+    patch "/users/:id", to: "user#update"
+  end
 end
